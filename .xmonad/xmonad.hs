fadaaeba1e70477f527b1417f36adf1c19d43cdc
Cary Robbins <carymrobbins@gmail.com>
2017-01-18 11:42:00 -0500

    Updated xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,14 @@
 import XMonad
+import XMonad.Actions.UpdatePointer
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Util.Run
 
 main = do
-  xmonad defaultConfig
+  xmonad def
     { terminal = "terminator"
     , modMask = mod4Mask
     , borderWidth = 1
     , startupHook = setWMName "LG3D"
+    , logHook = updatePointer (0.5, 0.5) (0, 0) -- Exact center of window
     }
