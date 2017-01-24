5f4770605be056cd08168332e87547a89f3f4085
Cary Robbins <carymrobbins@gmail.com>
2017-01-24 13:16:33 -0500

    Fixed 4-monitor xmonad bindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,19 +6,19 @@ import qualified XMonad.StackSet as W
 import           XMonad.Util.EZConfig
 import           XMonad.Util.Run
 
-myModKey = mod1Mask
+myModKey = mod4Mask
 
-myScreenKeys = zip [xK_w, xK_e, xK_r, xK_d] [0,1,2,0]
+myScreenKeys = zip [xK_w, xK_e, xK_r, xK_d] [3,1,2,0]
 
 myKeys =
   [((m .|. myModKey, key), screenWorkspace sc >>= flip whenJust (windows . f))
-    | (key, sc) <- screenKeys
+    | (key, sc) <- myScreenKeys
     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)] ]
 
 main = do
   xmonad $ def
     { terminal = "terminator"
-    , modMask = mod4Mask
+    , modMask = myModKey
     , borderWidth = 1
     , startupHook = setWMName "LG3D"
     , logHook = updatePointer (0.5, 0.5) (0, 0) -- Exact center of window
