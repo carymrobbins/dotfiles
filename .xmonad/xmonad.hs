e7e7c36fa633fd7b6da078f3eb6c073ff8189cb6
Cary Robbins <carymrobbins@gmail.com>
2017-01-24 12:10:52 -0500

    Attempt to add 4-monitor bindings to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,25 @@
-import XMonad
-import XMonad.Actions.UpdatePointer
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.SetWMName
-import XMonad.Util.Run
+import           XMonad
+import           XMonad.Actions.UpdatePointer
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.SetWMName
+import qualified XMonad.StackSet as W
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Run
+
+myModKey = mod1Mask
+
+myScreenKeys = zip [xK_w, xK_e, xK_r, xK_d] [0,1,2,0]
+
+myKeys =
+  [((m .|. myModKey, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    | (key, sc) <- screenKeys
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)] ]
 
 main = do
-  xmonad def
+  xmonad $ def
     { terminal = "terminator"
     , modMask = mod4Mask
     , borderWidth = 1
     , startupHook = setWMName "LG3D"
     , logHook = updatePointer (0.5, 0.5) (0, 0) -- Exact center of window
-    }
+    } `additionalKeys` myKeys
