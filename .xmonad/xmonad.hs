a35d0b5e25f8fd72f407d8c16d07e14e086e5acf
Cary Robbins <carymrobbins@gmail.com>
2017-01-26 08:01:16 -0500

    Updated xmonad to use default layoutHook

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,20 +11,6 @@ myModKey = mod4Mask
 
 myScreenKeys = zip [xK_w, xK_e, xK_r, xK_d] [3,1,2,0]
 
-myLayout = tiled ||| Mirror tiled ||| Full
-  where
-  -- default tiling algorithm partitions the screen into two panes
-  tiled   = Tall nmaster delta ratio
-
-  -- The default number of windows in the master pane
-  nmaster = 1
-
-  -- Default proportion of screen occupied by master pane
-  ratio   = 1/2
-
-  -- Percent of screen to increment by when resizing panes
-  delta   = 3/100
-
 myKeys =
   [((m .|. myModKey, key), screenWorkspace sc >>= flip whenJust (windows . f))
     | (key, sc) <- myScreenKeys
@@ -39,7 +25,7 @@ main = do
     , handleEventHook = def <+> docksEventHook
     , startupHook = setWMName "LG3D"
     , manageHook = def <+> manageDocks
-    , layoutHook = avoidStruts myLayout
+    , layoutHook = avoidStruts $ layoutHook def
     , logHook =
         updatePointer (0.5, 0.5) (0, 0) -- Exact center of window
         <+> dynamicLogWithPP xmobarPP { ppOutput = hPutStrLn xmobarProc }
