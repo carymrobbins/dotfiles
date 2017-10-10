daeb7a9439b3f4ce065cb39108ae648a6aa7b0b5
Cary Robbins <carymrobbins@gmail.com>
2017-10-10 09:15:08 -0500

    Non-working xmonad hamster update

--- linux/.xmonad/xmonad.hs
+++ linux/.xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import           XMonad.Actions.UpdatePointer
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.SetWMName
+import           XMonad.Util.NamedScratchpad
 import qualified XMonad.StackSet as W
 import           XMonad.Util.EZConfig
 import           XMonad.Util.Run
@@ -16,9 +17,37 @@ myModKey = mod4Mask
 myScreenKeys = zip [xK_w, xK_e, xK_r, xK_d] [1,0,2,0]
 
 myKeys =
-  [((m .|. myModKey, key), screenWorkspace sc >>= flip whenJust (windows . f))
+  -- Set screen keys
+  [
+    ((m .|. myModKey, key), screenWorkspace sc >>= flip whenJust (windows . f))
     | (key, sc) <- myScreenKeys
-    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)] ]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+  ] ++
+  -- Scratchpads
+  [ ((shiftMask .|. myModKey, xK_h), scratchHamster)
+  ]
+  where
+  scratchHamster = namedScratchpadAction myScratchpads hamsterScratchpadName
+
+hamsterScratchpadName = "hamster"
+
+myScratchpads =
+  [ NS "hamster" spawnHamster findHamster manageHamster
+  ]
+  where
+  spawnHamster = hamsterScratchpadName
+  findHamster = className =? "Time Tracker"
+  manageHamster = customFloating $ W.RationalRect h w t l
+    where
+    h = 0.6
+    w = 0.6
+    t = (1 - h) / 2 -- centered top/botom
+    l = (1 - w) / 2 -- centered left/right
+
+myManageHook =
+  def
+  <+> manageDocks
+  <+> namedScratchpadManageHook myScratchpads
 
 main = do
   xmobarProc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
@@ -29,7 +58,7 @@ main = do
     , focusedBorderColor = "#268bd2"
     , handleEventHook = def <+> docksEventHook
     , startupHook = setWMName "LG3D"
-    , manageHook = def <+> manageDocks
+    , manageHook = myManageHook
     , layoutHook = avoidStruts $ layoutHook def
     , logHook =
         updatePointer (0.5, 0.5) (0, 0) -- Exact center of window
